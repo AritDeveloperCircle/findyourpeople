760090ae19b2749910bcfdb97a70bd5ab747da50
Ibimina hart <ibimina.c.hart@gmail.com>
2023-04-19 13:26:51 +0100

    docs: add dummy data and iterate listings

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,42 @@
 import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import Listing from "@/components/landing/listing";
 import FooterBar from "@/common/FooterBar";
 import ReusableNavLink from "@/common/ReusableNavLink";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const listings = [
+    {
+      id: 1,
+      name: "arit developer circle",
+      manager: "Arit dev",
+      description:
+        "Arit developer circle is a community of developers who are passionate about building software products and services that solve real world problems.",
+    },
+    {
+      id: 2,
+      name: "arit developer circle",
+      manager: "Arit dev",
+      description:
+        "Arit developer circle is a community of developers who are passionate about building software products and services that solve real world problems.",
+    },
+    {
+      id: 3,
+      name: "arit developer circle",
+      manager: "Arit dev",
+      description:
+        "Arit developer circle is a community of developers who are passionate about building software products and services that solve real world problems.",
+    },
+    {
+      id: 4,
+      name: "arit developer circle",
+      manager: "Arit dev",
+      description:
+        "Arit developer circle is a community of developers who are passionate about building software products and services that solve real world problems.",
+    },
+  ];
+   
   return (
     <>
       <Head>
@@ -16,7 +45,11 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}></main>
+      <main className={styles.main}>
+        {listings.map((data) => (
+          <Listing data={data} key={data.id} />
+        ))}
+      </main>
       <FooterBar />
     </>
   );
