f9834eaaa5def3367b670adc2fa5cb6c796804b9
Taylor J <taylordjone@gmail.com>
2023-04-19 12:00:46 +0200

    docs: avatar component

--- src/components/CustomizableAvatar.js
+++ src/components/CustomizableAvatar.js
@@ -1,7 +1,14 @@
 import Image from 'next/image'
-function CustomizableAvatar({ src, height, width, alt, ...props }) {
+function CustomizableAvatar({
+  customClass,
+  src,
+  height,
+  width,
+  alt,
+  ...props
+}) {
   return (
-    <div>
+    <div className={customClass}>
       <Image {...props} src={src} height={height} width={width} alt={alt} />
     </div>
   )
