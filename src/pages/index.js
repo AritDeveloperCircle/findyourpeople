4a04407fc75e018aec0963a9ff36516ca62f86ed
Ibimina hart <ibimina.c.hart@gmail.com>
2023-04-20 07:24:10 +0100

    docs: remove dummy data and array iteration

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,39 +1,7 @@
 import Head from "next/head";
 import styles from "@/styles/Home.module.css";
-import Listing from "@/components/landing/listing";
 
 export default function Home() {
-  const listings = [
-    {
-      id: 1,
-      name: "arit developer circle",
-      manager: "Arit dev",
-      description:
-        "Arit developer circle is a community of developers who are passionate about building software products and services that solve real world problems.",
-    },
-    {
-      id: 2,
-      name: "arit developer circle",
-      manager: "Arit dev",
-      description:
-        "Arit developer circle is a community of developers who are passionate about building software products and services that solve real world problems.",
-    },
-    {
-      id: 3,
-      name: "arit developer circle",
-      manager: "Arit dev",
-      description:
-        "Arit developer circle is a community of developers who are passionate about building software products and services that solve real world problems.",
-    },
-    {
-      id: 4,
-      name: "arit developer circle",
-      manager: "Arit dev",
-      description:
-        "Arit developer circle is a community of developers who are passionate about building software products and services that solve real world problems.",
-    },
-  ];
-   
   return (
     <>
       <Head>
@@ -42,11 +10,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        {listings.map((data) => (
-          <Listing data={data}  key={data.id} />
-        ))}
-      </main>
+      <main className={styles.main}></main>
     </>
   );
 }
