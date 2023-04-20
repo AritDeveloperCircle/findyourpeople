881a283c7a655941724cb299a39931b25485b13e
Ibimina hart <ibimina.c.hart@gmail.com>
2023-04-20 07:24:10 +0100

    docs: remove dummy data and array iteration

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,37 +5,6 @@ import FooterBar from "@/common/FooterBar";
 import ReusableNavLink from "@/common/ReusableNavLink";
 
 export default function Home() {
-  const listings = [
-    {
-      id: 1,
-      name: "arit developer circle",
-      manager: "Arit dev",
-      description:
-        "Arit developer circle is a community of developers who are passionate about building software products and services that solve real world problems.",
-    },
-    {
-      id: 2,
-      name: "arit developer circle",
-      manager: "Arit dev",
-      description:
-        "Arit developer circle is a community of developers who are passionate about building software products and services that solve real world problems.",
-    },
-    {
-      id: 3,
-      name: "arit developer circle",
-      manager: "Arit dev",
-      description:
-        "Arit developer circle is a community of developers who are passionate about building software products and services that solve real world problems.",
-    },
-    {
-      id: 4,
-      name: "arit developer circle",
-      manager: "Arit dev",
-      description:
-        "Arit developer circle is a community of developers who are passionate about building software products and services that solve real world problems.",
-    },
-  ];
-   
   return (
     <>
       <Head>
@@ -45,9 +14,6 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        {listings.map((data) => (
-          <Listing data={data}  key={data.id} />
-        ))}
         {listings.map((data) => (
           <Listing data={data}  key={data.id} />
         ))}
