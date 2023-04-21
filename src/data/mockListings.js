bbe91466f43ce60a67bd8ea93c725cbe5067ecbb
Ibimina hart <ibimina.c.hart@gmail.com>
2023-04-21 18:31:31 +0100

    docs: mock data for listings

---
+++ src/data/mockListings.js
@@ -0,0 +1,30 @@
+export const listings = [
+  {
+    id: 1,
+    name: "arit dev circle",
+    manager: "arit dev",
+    description:
+      "this is a community for developers, we are here to help each other",
+  },
+  {
+    id: 2,
+    name: "dev circle",
+    manager: "justine dev",
+    description:
+      "this is a community for developers, we are here to help each other",
+  },
+  {
+    id: 3,
+    name: "dev circle",
+    manager: "Augustine dev",
+    description:
+      "this is a community for developers, we are here to help each other",
+  },
+  {
+    id: 4,
+    name: "dev circle",
+    manager: "arit dev",
+    description:
+      "this is a community for developers, we are here to help each other",
+  },
+];
