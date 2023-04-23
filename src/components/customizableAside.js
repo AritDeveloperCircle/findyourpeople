abbb9e249138b6ffdcae3ee51b2eb4c437129a4e
Ajewole seyi <sheyitofunmi22@gmail.com>
2023-04-23 13:16:41 +0100

    customizableAvatar

--- src/components/customizableAside.js
+++ src/components/customizableAside.js
@@ -2,10 +2,10 @@ import React from 'react';
 import Image from 'next/image';
 import styles from './aside.module.css';
 
-const CustomizableAside = ({CustomizedAvatar, asideTitle , asideParagraph,...props}) => {
+const CustomizableAside = ({CustomizableAvatar, asideTitle , asideParagraph,...props}) => {
     return (
         <div {...props}>
-            <div> {CustomizedAvatar}</div>
+            <div> {CustomizableAvatar}</div>
             <h2> {asideTitle} </h2>
             <p> {asideParagraph} </p>
         </div>
