a54b5a8021f0d10772666bf1af0ecaa727245305
Ibimina hart <ibimina.c.hart@gmail.com>
2023-04-24 21:23:18 +0100

    docs: remove empty stylesheet import

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import styles from "@/styles/Home.module.css";
 import Hero from "@/components/landing/Hero";
 
 export default function Home() {
@@ -11,7 +10,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
+      <main>
         <Hero />
       </main>
     </>
