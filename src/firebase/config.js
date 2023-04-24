4cd5bb612accaa023cb00db90105b65ff6a6732e
Ibimina hart <ibimina.c.hart@gmail.com>
2023-04-24 10:49:31 +0100

    docs: add firebase storage

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import {getStorage} from "firebase/storage"
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -14,5 +15,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const firebaseAuth = getAuth(app);
 const firebaseDb = getFirestore(app);
+const firebaseStorage = getStorage(app);
 
-export { firebaseAuth, firebaseDb };
+export { firebaseAuth, firebaseDb,firebaseStorage };
