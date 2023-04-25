9daca7feb48cac4b5d2633eee54fb39fa5e48180
Ajewole seyi <sheyitofunmi22@gmail.com>
2023-04-25 16:13:17 +0100

    doc:customizableAvatar File-update

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,10 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import customizableAvatar from "@/components/customizableAvatar";
 
 const inter = Inter({ subsets: ['latin'] })
+
 
 export default function Home() {
   return (
