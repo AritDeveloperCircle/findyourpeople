a06574017b88aaab1bbd4f898d2ba8ad6521421d
Ajewole seyi <sheyitofunmi22@gmail.com>
2023-04-25 15:10:54 +0100

    doc: aside files added

--- src/components/customizableAside.js
+++ src/components/customizableAside.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import Image from 'next/image';
-import styles from './aside.module.css';
+import styles from './customizableAside.module.css';
 
-const CustomizableAside = ({CustomizableAvatar, asideTitle , asideParagraph,...props}) => {
+function customizableAside() {
+
     return (
-        <div {...props}>
-            <div> {CustomizableAvatar}</div>
-            <h2> {asideTitle} </h2>
-            <p> {asideParagraph} </p>
-        </div>
+            <div className= {styles.aside-container}>
+                <div className= {styles.aside-img}>
+                    {customizableAvatar}
+                </div>
+                <h2>Lorem ipsum dolor sit amet</h2>
+                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam </p>
+            </div>
+        
         
     )
+
 }
+    
+
 
 export default CustomizableAside
