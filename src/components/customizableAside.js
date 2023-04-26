5585b14904f57e269a1cce5f22882f03e7fcaeda
Ajewole seyi <sheyitofunmi22@gmail.com>
2023-04-26 17:14:18 +0100

    AsideFile added

--- src/components/customizableAside.js
+++ src/components/customizableAside.js
@@ -2,15 +2,16 @@ import React from 'react';
 import Image from 'next/image';
 import styles from './customizableAside.module.css';
 
-function customizableAside() {
+function CustomizableAside() {
 
     return (
-            <div className= {styles.aside-container}>
-                <div className= {styles.aside-img}>
-                    {customizableAvatar}
+            <div className={styles.asideContainer}>
+                <div>
+                    <Image src="/images/customizableAvatar.svg" width="110" height="110" priority  alt=''/>
                 </div>
+
                 <h2>Lorem ipsum dolor sit amet</h2>
-                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam </p>
+                <p>Sed ut perspiciatis  Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam </p>
             </div>
         
         
