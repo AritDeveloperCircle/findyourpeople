b82ded5807f76208cb85162ef42a3784114bd34a
Ajewole seyi <sheyitofunmi22@gmail.com>
2023-05-04 07:36:58 +0100

    removed un-used styles

--- src/components/customizableAside.js
+++ src/components/customizableAside.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import Image from 'next/image';
-import styles from './customizableAside.module.css';
 import CustomizableAvatar from "./components/common/customavatar/CustomizableAvatar"
 
 function CustomizableAside({asideCustomClass, src, height, width, pContent, h2Content}) {
