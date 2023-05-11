aae85496758d4fc62adbea57cc62acca4f86d731
Ibimina hart <ibimina.c.hart@gmail.com>
2023-05-11 18:08:38 +0100

    docs: change obj property

--- src/components/landing/listing.js
+++ src/components/landing/listing.js
@@ -5,15 +5,19 @@ import styles from "./listing.module.css";
 function Listing({ data }) {
   return (
     <>
-       <article className={styles.listing}>
+      <article className={styles.listing}>
         <div className={styles.listing__image__container}>
-          <Image src="/image-placeholder.png" width={50} height={50} alt={`${data.name} avatar`} />
+          <Image
+            src="/image-placeholder.png"
+            width={50} height={50}
+            alt={`${data.community_name} avatar`}  />
         </div>
         <div className={styles.listing__content}>
           <h2 className={styles.listing__content__name}>{data.community_name}</h2>
-          <p className={styles.listing__content__manager}>{data.manager}</p>
-          <p className={styles.listing__content__description}>{data.description}</p>
-          <Link href={`/${encodeURIComponent(data.id)}`} className={styles.listing__view__more}>view more</Link>
+          <p className={styles.listing__content__manager}> {data.community_manager} </p>
+          <p className={styles.listing__content__description}> {data.community_description}</p>
+          <Link href={`/${encodeURIComponent(data.communitd_id)}`}  className={styles.listing__view__more}>view more
+          </Link>
         </div>
       </article>
     </>
