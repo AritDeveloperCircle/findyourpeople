7bc92025092452c7a72254394bf9dbf2ba531cf3
Taylor J <taylordjone@gmail.com>
2023-05-13 18:32:38 +0200

    styling signupform on signup page with Tailwind

--- src/components/signupform/SignupFormContainer.js
+++ src/components/signupform/SignupFormContainer.js
@@ -1,29 +1,31 @@
-import Link from "next/link";
 import styles from "./signupcontainer.module.css";
 import CustomizableAvatar from "../common/customavatar/CustomizableAvatar";
 import SignupForm from "./SignupForm";
 
 function SignupFormContainer() {
-  const AVATAR_HEIGHT = 100;
-  const AVATAR_WIDTH = 100;
+  const AVATAR_HEIGHT = 50;
+  const AVATAR_WIDTH = 50;
   return (
     <main>
-      <div className={styles.div}>
-        <CustomizableAvatar
-          height={AVATAR_HEIGHT}
-          width={AVATAR_WIDTH}
-          src="/image-placeholder.png"
-          alt="main avatar"
-        />
-      </div>
+      <CustomizableAvatar
+        customClass="flex justify-center items-center mb-5"
+        height={AVATAR_HEIGHT}
+        width={AVATAR_WIDTH}
+        src="/image-placeholder.png"
+        alt="main avatar"
+      />
+
       <h2 className={styles.header}>Create an account</h2>
       <h4 className={styles.sub}>
         Lets get started with creating your community!
       </h4>
       <SignupForm />
-      <div className={styles.login}>
+      <div className="text-center mt-4">
         <p>
-          Have an account? <a href="#">Login</a>{" "}
+          Have an account?
+          <a className="text-sky-600 ml-1" href="#">
+            Login
+          </a>
         </p>
       </div>
     </main>
