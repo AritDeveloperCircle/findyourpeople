f884208e5249cdc1101a98f11b3a95197a38cb3c
Taylor J <taylordjone@gmail.com>
2023-05-16 10:03:08 +0200

    styling Signup page

--- src/components/signupform/SignupForm.js
+++ src/components/signupform/SignupForm.js
@@ -1,79 +1,77 @@
-import { useState } from 'react'
-import CustomizableButton from '../common/CustomizableButton'
-import styles from './signupform.module.css'
+import { useState } from "react";
+import CustomizableButton from "../common/CustomizableButton";
+import styles from "./signupform.module.css";
 
 function Form() {
   const [user, setUser] = useState({
-    name: '',
-    email: '',
-    password: '',
-  })
+    name: "",
+    email: "",
+    password: "",
+  });
   const handleChange = (e) => {
-    setUser({ ...user, [e.target.name]: e.target.value })
-  }
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
 
   const handleSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     if (validateForm()) {
-      console.log('Form submitted sucessfully')
+      console.log("Form submitted sucessfully");
       // Will direct the user to the Login Form
       setTimeout(() => {
-        resetForm()
-      }, 1000)
+        resetForm();
+      }, 1000);
     }
-  }
+  };
 
   const resetForm = () => {
-    setUser({ name: '', email: '', password: '' })
-  }
+    setUser({ name: "", email: "", password: "" });
+  };
 
   const validateForm = () => {
     if (!user.name || !user.email || !user.password) {
-      alert('please fill out all required fields.')
-      return false
+      alert("please fill out all required fields.");
+      return false;
     }
 
-    const emailPattern = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/
+    const emailPattern = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
     if (!user.email.match(emailPattern)) {
-      alert('please enter a valid email address')
-      return false
+      alert("please enter a valid email address");
+      return false;
     }
 
-    return true
-  }
+    return true;
+  };
 
   return (
     <div>
       <form className={styles.form} onSubmit={handleSubmit}>
         {/* name */}
         <div className={styles.formRow}>
-          <label htmlFor='name' className={styles.formLabel}>
-            Name<span>*</span>
+          <label htmlFor="name" className={styles.formLabel}>
+            Name
           </label>
           <input
-            type='text'
+            type="text"
             className={styles.formInput}
-            id='name'
-            name='name'
-            aria-label='name'
+            id="name"
+            name="name"
+            aria-label="name"
             value={user.name}
             onChange={handleChange}
             required
-            minLength={2}
-            maxLength={50}
           />
         </div>
         {/* email */}
         <div className={styles.formRow}>
-          <label htmlFor='email' className={styles.formLabel}>
-            Email<span>*</span>
+          <label htmlFor="email" className={styles.formLabel}>
+            Email
           </label>
           <input
-            type='email'
+            type="email"
             className={styles.formInput}
-            id='email'
-            name='email'
-            aria-label='email'
+            id="email"
+            name="email"
+            aria-label="email"
             value={user.email}
             onChange={handleChange}
             required
@@ -81,30 +79,28 @@ function Form() {
         </div>
         {/* password */}
         <div className={styles.formRow}>
-          <label htmlFor='password' className={styles.formLabel}>
-            Password<span>*</span>
+          <label htmlFor="password" className={styles.formLabel}>
+            Password
           </label>
           <input
-            type='password'
+            type="password"
             className={styles.formInput}
-            id='password'
-            name='password'
-            aria-label='password'
+            id="password"
+            name="password"
+            aria-label="password"
             value={user.password}
             onChange={handleChange}
             required
-            minLength={6}
-            maxLength={20}
           />
         </div>
         <CustomizableButton
           customClass={styles.btn}
-          text='SIGN UP'
-          aria-label='Sign up button'
+          text="SIGN UP"
+          aria-label="Sign up button"
         />
       </form>
     </div>
-  )
+  );
 }
 
-export default Form
+export default Form;
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -2,19 +2,18 @@ import CustomizableAside from "@/components/common/customizableAside";
 import SignupFormContainer from "@/components/signupform/SignupFormContainer";
 
 export default function signup() {
-  const AVATAR_HEIGHT = 100;
-  const AVATAR_WIDTH = 100;
-  const photo = "/image-placeholder.png";
-  const header = "Lorem ipsum dolor sit amet";
-  const info =
-    "Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet";
+  const AVATAR_HEIGHT = 350;
+  const AVATAR_WIDTH = 350;
+  const photo = "/Signupimage.png";
+  const header = "You're just one step away from an amazing experience";
+  const info = "Let's get you started with joining our community";
 
   return (
-    <main className="grid grid-cols-2 mt-10">
+    <main className="grid grid-cols-2 min-h-screen">
       <section>
         <SignupFormContainer />
       </section>
-      <section className="bg-zinc-300">
+      <section className="bg-cover bg-[url('/SignupBackground.jpg')]">
         <div className="text-center">
           <CustomizableAside
             height={AVATAR_HEIGHT}
