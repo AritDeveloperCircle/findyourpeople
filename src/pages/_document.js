fc37ba8fbc5eb2ab401abe17783b4c00145e5c5b
Taylor J <taylordjone@gmail.com>
2023-05-16 18:51:19 +0200

    Updated font using NextJs package

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,12 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Urbanist:wght@100;200;300;400;500;600&display=swap"
-          rel="stylesheet"
-        ></link>
-      </Head>
+      <Head></Head>
       <body>
         <Main />
         <NextScript />
