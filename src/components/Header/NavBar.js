35d6a0a01a9a08e86d559545f3f7a7f0d3bc88ef
Neka Blair <ourblairfamily@gmail.com>
2023-05-18 00:21:13 -0500

    style nav bar

--- src/components/Header/NavBar.js
+++ src/components/Header/NavBar.js
@@ -2,36 +2,37 @@ import Link from 'next/link';
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from './NavBar.module.css';
+import ReusableNavLink from '../common/ReusableNavLink';
 
 export default function NavBar() {
     return (
-        <div className={styles.container}>
+        <div class=" relative flex items-center justify-evenly w-full text-lg py-4 px-24">
             <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
+                src="/LOGO.png"
+                alt="Find Your People Tech Logo"
                 className={styles.vercelLogo}
-                width={100}
-                height={24}
+                width={80}
+                height={28}
                 priority
               />
-            
-            <form>
-                <input id="searchInputBar" type="text" placeholder="Search" name='search' required/>
-                <button type='submit'>
-                    <Image
-                        src="/search-glass.svg"
-                        alt="Search Image"
-                        className={styles.searchImage}
-                        width={20}
-                        height={20}
-                    />
-                </button>
-            </form>
-            
-            <ul className={styles.nav}>
-                <li>Create Listing</li>
-                <li>Log Out</li>
-            </ul>
+            <div class="flex items-center justify-between gap-10">
+                <Link href="./index.js" class="text-slate-700 hover:text-blue-500 cursor-pointer">Home</Link>
+                <Link href="#" class="text-slate-700 text-base hover:text-blue-500 cursor-pointer">About Us</Link>
+                <Link href="#" class="text-slate-700 text-base hover:text-blue-500 cursor-pointer">Contact</Link>
+                <form class="flex items-center border-2 border-solid border-gray-300 rounded-xl border-r-0" role="search">
+                    <div class="flex gap-3 px-3">
+                        <Image
+                            src="/search-glass.svg"
+                            alt="Search Image"
+                            className={styles.searchImage}
+                            width={20}
+                            height={20}
+                        />
+                        <input id="searchInputBar" type="text" placeholder="Search for..." name='search' aria-label="Search through communities" size="40" required/>
+                    </div>
+                    <button type='submit' class="bg-gradient-to-r from-blue-800 to-sky-950 text-white py-2 px-6 rounded-xl hover:bg-gray-500 active:bg-gray-600 focus: outline-none focus:ring-2 focus:ring-blue-300">SEARCH</button>
+                </form>
+            </div>
         </div>
     );
 }
