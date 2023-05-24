0307487e0a9b3c53fc677042d44cecc8ec1b233e
Neka Blair <ourblairfamily@gmail.com>
2023-05-23 22:24:37 -0500

    add hero and style manager dashboard

--- src/pages/managerDashboard.js
+++ src/pages/managerDashboard.js
@@ -6,6 +6,7 @@ import CustomizableAvatar from '@/components/common/customavatar/CustomizableAva
 import CustomizableButton from '@/components/common/CustomizableButton';
 import FooterBar from '@/components/common/FooterBar';
 import DashboardListing from '@/components/singledashboardlisting/DashboardListing';
+import styles from '../styles/managerDashboard.module.css';
 
 function managerDashboard () {
   return (
@@ -13,29 +14,33 @@ function managerDashboard () {
         <NavBar />
 
         <main className='bg-white'>
-            <div className='bg-sky-200 p-20 m-20 rounded-md flex justify-between'>
-                <div className=''>
-                    <h1 className='text-5xl'>Welcome Back, Emmanuel!</h1>
-                    <p>You have 8 new members!</p>
-                    <h3>Edit Profile</h3>
+            <div className='bg-sky-200 m-20 rounded-md flex justify-between sm-text-center'>
+                <div className='flex flex-col gap-3 p-20'>
+                    <h1 className='text-5xl text-blue-800'>Welcome Back, Emmanuel!</h1>
+                    <p className='text-2xl'>You have 8 new members!</p>
+                    <div className='pt-10'>
+                        <Link href="#" class="text-slate-700 text-base hover:text-blue-500 cursor-pointer text-2xl">Edit Profile</Link>
+                    </div>
                 </div>
-                <div>
-                    <Image
-                        src="./profile/manager-dashboard-character.png"
+                <div className='pr-40 pt-8 md:flex lg:shrink-0 '>
+                    <Image className='min-[320px]:text-center max-[1240px]:inherit'
+                        src="/manager-dashboard-character.png"
                         alt="manager character"
-                        width={80}
-                        height={28}
+                        width={500}
+                        height={500}
                     />
                 </div>
             </div>
 
-            <div className='bg-sky-200 text-center p-20 m-20 rounded-md'>
+            <div className='bg-slate-200 text-center p-20 m-20 rounded-md'>
                 <h1 className='text-5xl'>Community Dashboard</h1>
-                    <div className='grid grid-cols-2 place-content-center p-40'>
-                        <div className='w-screen'>
+                    <div className='flex flex-row justify-stretch min-[320px]:text-center max-[1240px]:inherit'
+                    // 'grid grid-cols-2 place-content-center p-40'
+                    >
+                        <div className='flex-initial '>
                             <DashboardListing />
                         </div>
-                        <div className='w-screen'>
+                        <div className='flex-initial '>
                             <DashboardListing />
                         </div>
                         
