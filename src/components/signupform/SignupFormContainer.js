e75b77111a20a0d3fc45d9fa80e07145f9e3fac5
Ibimina hart <ibimina.c.hart@gmail.com>
2023-05-30 10:22:45 +0100

    style: remove height specification on main element, remove br tag and aligned text center, remove extra whitespace

--- src/components/signupform/SignupFormContainer.js
+++ src/components/signupform/SignupFormContainer.js
@@ -3,19 +3,19 @@ import SignupForm from "./SignupForm";
 
 function SignupFormContainer() {
   return (
-    <main className="mt-24 h-screen">
+    <main className="lg:max-w-lg lg:w-11/12">
       <div className="text-center ">
         <h2 className="text-grey-dark font-bold text-4xl tracking-normal">
           Create an account
         </h2>
-        <h4 className="text-grey-dark tracking-wider text-sm opacity-80 mb-4">
-          Lets get started with <br /> creating your community
+        <h4 className="text-grey-dark text-center  tracking-wider text-sm opacity-80">
+          Lets get started with creating your community
         </h4>
       </div>
 
       <SignupForm />
       <div className="text-center mt-4">
-        <p className="text-gray-500 text-md mt-40">
+        <p className="text-gray-500 text-md">
           Have an account?
           <Link href="/">
             <span className="text-primary ml-2 font-semibold">Sign in</span>
