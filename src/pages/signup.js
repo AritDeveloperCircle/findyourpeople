fa45f3ed4ad1c1b4be5f31ae1a85cdf91882fe1b
Ibimina hart <ibimina.c.hart@gmail.com>
2023-05-30 10:23:55 +0100

    style: reduce avatar height, add aside section on small screen

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -2,26 +2,26 @@ import CustomizableAside from "@/components/common/customizableAside";
 import SignupFormContainer from "@/components/signupform/SignupFormContainer";
 
 export default function signup() {
-  const AVATAR_HEIGHT = 400;
-  const AVATAR_WIDTH = 400;
+  const AVATAR_HEIGHT = 300;
+  const AVATAR_WIDTH = 300;
   const photo = "/Signupimage.png";
   const header = "You are just a step away from an";
   const span = " amazing experience!";
   const info = "Let's get you started with joining our community";
 
   return (
-    <main className="grid grid-cols-2 h-screen">
-      <section>
+    <main className="grid items-center md:items-stretch md:grid-cols-2 h-screen">
+      <section className={`flex items-center justify-center `}>
         <SignupFormContainer />
       </section>
-      <section className="bg-cover  bg-[url('/AsideBackground.png')] flex justify-center">
+      <section className="hidden  md:bg-cover  md:bg-[url('/AsideBackground.png')] md:flex lg:justify-center">
         <div className="flex flex-col items-center justify-center px-14">
           <CustomizableAside
             height={AVATAR_HEIGHT}
             width={AVATAR_WIDTH}
             alt="Sign Up Logo"
             src={photo}
-            avatarCustomClass="flex justify-center mb-28"
+            avatarCustomClass="flex justify-center mb-5"
             headerText={header}
             spanText={span}
             paragraphText={info}
