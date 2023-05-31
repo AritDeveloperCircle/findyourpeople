6f497d797481c6e4c4333bb9d94f9ba0ccc10f02
Neka Blair <ourblairfamily@gmail.com>
2023-05-30 22:34:38 -0500

    add extra width to cards, removed unused imports, add extra line

--- src/pages/managerDashboard.js
+++ src/pages/managerDashboard.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import computerPicture from '/public/computer-pic.png'
 import NavBar from '@/components/Header/NavBar';
-import CustomizableAvatar from '@/components/common/customavatar/CustomizableAvatar';
-import CustomizableButton from '@/components/common/CustomizableButton';
 import FooterBar from '@/components/common/FooterBar';
 import DashboardListing from '@/components/singledashboardlisting/DashboardListing';
-import styles from '../styles/managerDashboard.module.css';
+// import styles from '../styles/managerDashboard.module.css';
 
 function managerDashboard () {
   return (
     <div>
         <NavBar />
-
         <main className='bg-white container mx-auto p-10'>
             <div className='bg-primary-lite my-10 rounded-md flex justify-between sm-text-center'>
                 <div className='flex flex-col gap-5 p-10  lg:text-left'>
@@ -36,24 +32,20 @@ function managerDashboard () {
 
             <div className='bg-grey-lite p-10 rounded-md'>
                 <h1 className='text-5xl p-10 text-center'>Community Dashboard</h1>
-                    <div className="grid grid-cols-2 col-span-1 auto-rows-max justify-items-center sm:grid-cols-1 lg:grid-cols-2 my-10">
-                        <div className='col-span-1'>
+                    <div className="grid grid-cols-2 col-span-1 auto-rows-max justify-items-center sm:grid-cols-1 xl:grid-cols-2 gap-10 my-10">
+                        <div className='col-span-1 pb-6'>
                             <DashboardListing 
                             className="" />
                         </div>
-                        <div className='col-span-1'>
+                        <div className='col-span-1 pb-6'>
                             <DashboardListing />
                         </div>
                     </div>
                 <h2 className='text-2xl text-center'>+ Add new community</h2>
             </div>
         </main>
-        
-        
         <FooterBar />
     </div>
-    
-
   )
 }
 
