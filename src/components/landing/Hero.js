40a81f319207844b6ac4f2e7d345b8a15ad2466b
Neka Blair <ourblairfamily@gmail.com>
2023-06-03 23:25:11 -0500

    refactor code for better responsiveness

--- src/components/landing/Hero.js
+++ src/components/landing/Hero.js
@@ -4,22 +4,24 @@ import styles from "./hero.module.css";
 
 function Hero() {
   return (
-    <section className="bg-white">
-      <div className="flex flex-row items-center gap-20 px-96 py-36 sm:text-left md:text-left ">
+    <section className="bg-white flex flex-col justify-center">
+      <div className="
+      flex items-center gap-20 
+      lg:px-96 py-36 sm:px-10">
         <div>
           <Image 
             src="/arrow.png"
-            className="pt-44 overflow sm:overflow-hidden lg:visible"
+            className="pt-44 sm:invisible lg:visible"
             height={100}
             width={150}
             alt="looping arrow pointing downwards"
           />
         </div>
-        <div className="flex flex-col items-center gap-12 justify-center">
-          <div className="text-6xl text-center">
+        <div className="flex flex-col items-center space-y-10 justify-center md:text-sm">
+          <div className="text-6xl text-center ">
             <h1>Join the <span className="text-primary bold">Tech Community</span>and get to discover people from all around the world!</h1>
           </div>
-          <p className="px-44 text-xl md:px-2 lg:visible text-center">Get to find different techies across the globe, build connections, interactions and networks by joining the community of developers and designers.</p>
+          <p className="px-4 text-xl md:px-2 lg:visible text-center sm:px-20  xl:px-20">Get to find different techies across the globe, build connections, interactions and networks by joining the community of developers and designers.</p>
           <div className="flex justify-center">
             <Link href="/register" className="bg-primary rounded-lg text-white text-2xl px-10 py-3 ">
               Click to Register
@@ -39,8 +41,8 @@ function Hero() {
           
       </div>
       
-      <div className="bg-[url('/bkgd-overlay-image.png')] bg-cover bg-no-repeat  bg-center ">
-        <div className="flex p-20 justify-evenly gap-6">
+      <div className="bg-[url('/bkgd-overlay-image.png')] bg-cover bg-no-repeat bg-center ">
+        <div className="flex flex-col p-20 justify-evenly gap-6 items-center xl:flex-row">
           <Image 
             src="/woman-smile.png"
             height={500}
@@ -63,7 +65,7 @@ function Hero() {
             alt="person sketching on notepad"
           />
         </div>
-        <div className="flex justify-center pt-1 pb-12">
+        <div className="flex justify-center pt-1 pb-12 px-14">
         <Image 
           src="/hero-text.png"
           height={300}
