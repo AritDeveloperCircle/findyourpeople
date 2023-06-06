11f44a583d8786c27bde61fcc611e3c0d4571d8c
Taylor J <taylordjone@gmail.com>
2023-06-06 15:58:45 +0200

    Saving Manager's Details

--- src/composables/authSignUp.js
+++ src/composables/authSignUp.js
@@ -1,18 +1,46 @@
-import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
-import { firebaseAuth } from "@/firebase/config";
+import {
+  createUserWithEmailAndPassword,
+  updateProfile,
+  fetchSignInMethodsForEmail,
+} from "firebase/auth";
+import { firebaseAuth, firebaseDb } from "@/firebase/config";
 import { useState } from "react";
+import { doc, setDoc } from "firebase/firestore";
 
 function useAuthSignUp() {
   const [addUser, setAddUser] = useState("");
   const [firebaseError, setFirebaseError] = useState("");
+
   const signup = ({ name, email, password }) => {
-    createUserWithEmailAndPassword(firebaseAuth, email, password)
-      .then((userCredential) => {
-        updateProfile(userCredential.user, { displayName: name });
-      })
-      .then((userCredential) => {
-        const user = userCredential.user;
-        setAddUser(user);
+    fetchSignInMethodsForEmail(firebaseAuth, email)
+      .then((signInMethods) => {
+        // I was trying to catch the email error here, but I already have in place using the firebaseError
+        if (signInMethods.length > 0) {
+          setFirebaseError("Email is already in use.");
+        } else {
+          createUserWithEmailAndPassword(firebaseAuth, email, password)
+            .then((userCredential) => {
+              const user = userCredential.user;
+              updateProfile(user, { displayName: name })
+                .then(() => {
+                  const userDocRef = doc(firebaseDb, "MANAGERS", user.uid);
+                  const userData = {
+                    displayName: user.displayName,
+                    email: user.email,
+                  };
+                  return setDoc(userDocRef, userData);
+                })
+                .then(() => {
+                  setAddUser(user);
+                })
+                .catch((error) => {
+                  setFirebaseError(error.message);
+                });
+            })
+            .catch((error) => {
+              setFirebaseError(error.message);
+            });
+        }
       })
       .catch((error) => {
         setFirebaseError(error.message);
