60d6c0cb2f7be1575a9443609b66074b47193407
Ibimina hart <ibimina.c.hart@gmail.com>
2023-06-06 10:59:08 +0100

    docs: remove unused import

--- src/pages/managerDashboard.js
+++ src/pages/managerDashboard.js
@@ -1,26 +1,21 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import computerPicture from '/public/computer-pic.png'
 import NavBar from '@/components/Header/NavBar';
-import CustomizableAvatar from '@/components/common/customavatar/CustomizableAvatar';
-import CustomizableButton from '@/components/common/CustomizableButton';
 import FooterBar from '@/components/common/FooterBar';
 import DashboardListing from '@/components/singledashboardlisting/DashboardListing';
-import styles from '../styles/managerDashboard.module.css';
 
 function managerDashboard () {
   return (
     <div>
         <NavBar />
-
         <main className='bg-white container mx-auto p-10'>
             <div className='bg-primary-lite my-10 rounded-md flex justify-between sm-text-center'>
                 <div className='flex flex-col gap-5 p-10  lg:text-left'>
                     <h1 className='text-5xl text-blue-800'>Welcome Back, Emmanuel!</h1>
                     <p className='text-2xl'>You have 8 new members!</p>
                     <div className='pt-10'>
-                        <Link href="#" class="text-slate-700 text-base hover:text-blue-500 cursor-pointer text-2xl">Edit Profile</Link>
+                        <Link href="#" class="text-slate-700 hover:text-blue-500 cursor-pointer text-2xl">Edit Profile</Link>
                     </div>
                 </div>
                 <div className='pr-40 pt-8 md:flex lg:shrink-0 '>
@@ -47,13 +42,9 @@ function managerDashboard () {
                     </div>
                 <h2 className='text-2xl text-center'>+ Add new community</h2>
             </div>
-        </main>
-        
-        
+        </main>      
         <FooterBar />
     </div>
-    
-
   )
 }
 
