89360a40d5df74b4ac119c215695509f697ca90d
Ojoachele Onuh <ejigboojoachele@gmail.com>
2023-06-06 15:36:40 +0100

    Remove community avatar

--- src/components/landing/listing.js
+++ src/components/landing/listing.js
@@ -3,19 +3,9 @@ import Image from "next/image";
 import styles from "./listing.module.css";
 
 function Listing({ data }) {
-  const avatarWidth = 50;
-  const avatarHeight = 50;
   return (
     <>
       <article className={styles.listing}>
-        <div className={styles.listing__image__container}>
-          <Image
-            src="/image-placeholder.png"
-            width={avatarWidth}
-            height={avatarHeight}
-            alt={`${data.community_name} avatar`}
-          />
-        </div>
         <div className={styles.listing__content}>
           <h2 className={styles.listing__content__name}>
             {data.community_name}
