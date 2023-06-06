d6aaa2dbc1437338da80610c049a6dbadd5965f0
Ibimina hart <ibimina.c.hart@gmail.com>
2023-06-06 13:23:35 +0100

    style: update styles

--- src/components/landing/listing.js
+++ src/components/landing/listing.js
@@ -7,17 +7,17 @@ function Listing({ data }) {
   const avatarHeight = 109
   return (
     <>
-      <article className="bg-grey-lite rounded-lg drop-shadow-md flex justify-content-center font-sans">
+      <article className="bg-grey-lite rounded-lg drop-shadow-md flex justify-content-center gap-3 p-4 font-sans">
         <div className={styles.listing__image__container}>
-          <Image className="ml-8 mt-6 rounded-full"
+          <Image className="rounded-full"
             src="/image-placeholder.png"
             width={avatarWidth} height={avatarHeight}
             alt={`${data.community_name} avatar`}  />
         </div>
-        <div className=" ml-12 mb-8 mr-8 mt-8 rounded-lg">
-          <h2 className="text-primary-dark capitalize font-medium text-2xl ">{data.community_name}</h2>
-          <p className="italic text-gradient-lite-blue capitalize text-xl font-medium"> {data.community_manager} </p>
-          <p className="text-grey-dark font-normal text-xl"> {data.community_description}</p>
+        <div className=" rounded-lg">
+          <h2 className="text-primary-dark capitalize font-medium text-2xl fo">{data.community_name}</h2>
+          <p className="italic text-gradient-lite-blue capitalize text-xl font-normal"> {data.community_manager} </p>
+          <p className="text-grey-dark font-normal text-xl my-6"> {data.community_description}</p>
           <Link href={`/${encodeURIComponent(data.communitd_id)}`} className="text-gradient-lite-blue uppercase text-xs">view more &gt;
           </Link>
         </div>
