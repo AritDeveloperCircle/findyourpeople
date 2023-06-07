84bc009b2f02f5841c0c664533c84b258674af44
Ibimina hart <ibimina.c.hart@gmail.com>
2023-06-07 15:45:14 +0100

    docs: remove router barcket, update condition

--- src/pages/managerDashboard.js
+++ src/pages/managerDashboard.js
@@ -3,13 +3,14 @@ import { useRouter } from "next/router";
 import useAuthContext from "@/context/useAuthContext";
 const ManagerDashboard = () => {
   const { state } = useAuthContext();
-  const { router } = useRouter();
+  const  router = useRouter();
 
   useEffect(() => {
-    if (state?.user?.uid.length <= 0) {
-      router.push("/login");
+    if (state?.user ===undefined || state?.user?.uid === "") {
+    router.push("/");
     }
-  });
+ 
+  },);
   return <div>managerDashboard</div>;
 };
 
