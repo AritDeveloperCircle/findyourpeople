102a1e8054a6ec729441990001f61ed18c7a5f62
Ibimina hart <ibimina.c.hart@gmail.com>
2023-06-08 12:38:22 +0100

    docs: update user state

--- src/composables/authLogIn.js
+++ src/composables/authLogIn.js
@@ -1,23 +1,27 @@
 import { signInWithEmailAndPassword } from "firebase/auth";
-import { firebaseAuth } from "@/firebase/config";
+import { firebaseAuth, firebaseDb } from "@/firebase/config";
 import { useState } from "react";
+import useAuthContext from "@/context/useAuthContext";
 
 function useAuthLogIn() {
+  const { dispatch } = useAuthContext();
 
-    const [addUser, setAddUser] = useState("");
-    const [firebaseError, setFirebaseError] = useState("")
+  const [firebaseError, setFirebaseError] = useState("");
 
-    const LogIn = ({  email, password }) => {
-      signInWithEmailAndPassword(firebaseAuth, email, password)
-          .then((userCredential) => {
-            setAddUser(userCredential.user)
-            //  Handle successful login
-          })
-          .catch((error) => {
-            setFirebaseError(error.message)
-        });
-    };
-    return { addUser, firebaseError, LogIn };
+  const LogIn = ({ email, password }) => {
+    signInWithEmailAndPassword(firebaseAuth, email, password)
+      .then(async (userCredential) => {
+        // setAddUser(userCredential.user)
+        const docRef = doc(firebaseDb, "MANAGERS", userCredential?.user?.uid);
+        const docSnap = await getDoc(docRef);
+        dispatch({ type: "SIGN_IN", payload: docSnap.data() });
+        //  Handle successful login
+      })
+      .catch((error) => {
+        setFirebaseError(error.message);
+      });
+  };
+  return { firebaseError, LogIn };
 }
 
 export { useAuthLogIn };
