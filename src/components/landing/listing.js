2a148d217f3b3d970143d09bae27e9645c0188e2
Chibuzo Idia <caren.vhr@gmail.com>
2023-06-10 18:26:28 +0100

    added responsiveness

--- src/components/landing/listing.js
+++ src/components/landing/listing.js
@@ -7,18 +7,18 @@ function Listing({ data }) {
   const avatarHeight = 109
   return (
     <>
-      <article className="bg-grey-lite rounded-lg drop-shadow-md flex justify-content-center font-sans">
+      <article className="bg-grey-lite my-2.5 mx-4 px-13 max-w-xl md:max-w-3xl lg:max-w-5xl h-85 lg:max-h-max rounded-2xl drop-shadow-md flex content-center font-sans">
         <div className={styles.listing__image__container}>
           <Image className="ml-8 mt-6 rounded-full"
             src="/image-placeholder.png"
             width={avatarWidth} height={avatarHeight}
             alt={`${data.community_name} avatar`}  />
-        </div>
-        <div className=" ml-12 mb-8 mr-8 mt-8 rounded-lg">
+        </div> 
+        <div className="ml-12 mt-8 mb-4 mr-8 md:mb-4 w-89 h-29 font-normal text-xl tracking-wide text-gray-dark">
           <h2 className="text-primary-dark capitalize font-medium text-2xl ">{data.community_name}</h2>
           <p className="italic text-gradient-lite-blue capitalize text-xl font-medium"> {data.community_manager} </p>
           <p className="text-grey-dark font-normal text-xl"> {data.community_description}</p>
-          <Link href={`/${encodeURIComponent(data.communitd_id)}`} className="text-gradient-lite-blue uppercase text-xs">view more &gt;
+          <Link href={`/${encodeURIComponent(data.communitd_id)}`} className="text-gradient-lite-blue uppercase text-xs mt-8">view more &gt;
           </Link>
         </div>
       </article>
