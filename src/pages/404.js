d0fb7b84f479dbe209376a344961763a92a85917
Neka Blair <ourblairfamily@gmail.com>
2023-06-12 11:22:09 -0500

    first commit

---
+++ src/pages/404.js
@@ -0,0 +1,3 @@
+export default function Custom404() {
+    return <h1 className="text-7xl py-80 text-footer-grey font-light flex justify-center items-center bg-gradient-to-r from-blue-500 min-h-screen">404 - Page Not Found</h1>
+}
