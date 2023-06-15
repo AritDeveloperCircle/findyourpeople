e63ddb6c64893e3baca2b4e56e889ff36e45976a
Ibimina hart <ibimina.c.hart@gmail.com>
2023-06-15 15:51:08 +0100

    docs: responsive navbar

--- src/components/Header/NavBar.js
+++ src/components/Header/NavBar.js
@@ -7,13 +7,15 @@ export default function NavBar({ fetchData }) {
 
   const navList = [
     { text: "About", href: "/about" },
+    { text: "home", href: "/" },
+    { text: "Contact", href: "/contact" },   
   ];
 
   const { error } = useCollection();
 
   return (
-    <div className=" relative flex items-center justify-evenly sm:text-left md:flex py-4 px-9 lg:flex-row flex-col">
-      <Link href="/">
+    <div className="grid grid-cols-4 p-4 items-center gap-3">
+      <Link href="/" className="">
         <Image
           src="/LOGO.png"
           alt="Find Your People Tech Logo"
@@ -23,36 +25,34 @@ export default function NavBar({ fetchData }) {
           priority
         />
       </Link>
-      <div className="flex gap-6">
+
+      <div className="relative mt-2  col-span-4 row-span-2 md:col-span-2 md:mt-0 md:row-span-1">
+        <Image
+          src="/search-glass.svg"
+          alt="Search Image"
+          width={20}
+          height={20}
+          className="absolute top-4 left-3"
+        />
+        {error && <p>{error}</p>}
+        <input
+          className="px-8 py-2 w-full border-2 border-solid border-gray-300 rounded-lg "
+          id="searchInputBar"
+          type="text"
+          placeholder="Search for..."
+          name="search"
+          aria-label="Search through communities"
+          size="35"
+          required
+          onChange={fetchData}
+        />
+      </div>
+      <div className="flex gap-6 ">
         <ul className="flex items-center justify-between gap-10">
-            {navList.map(({ text, href }) => (
-              <ReusableNavLink key={href} text={text} href={href} />
-            ))}
+          {navList.map(({ text, href }) => (
+            <ReusableNavLink key={href} text={text} href={href} />
+          ))}
         </ul>
-        <div className=" flex px-3 min-w-fit relative">
-          <div className="mr-2 absolute left-5 top-1/2 transform -translate-y-1/2">
-            <Image
-              src="/search-glass.svg"
-              alt="Search Image"
-              width={20}
-              height={20}
-            />
-          </div>
-
-          {error && <p>{error}</p>}
-          <input
-            className="pl-8 h-10 border-2 border-solid border-gray-300 rounded-lg "
-            id="searchInputBar"
-            type="text"
-            placeholder="Search for..."
-            name="search"
-            aria-label="Search through communities"
-            size="35"
-            required
-            onChange={fetchData}
-          />
-
-        </div>
       </div>
     </div>
   );
