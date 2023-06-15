ab1d1234e2d21b6cb26d11a3e9e9ee3ebba5ada2
Ibimina hart <ibimina.c.hart@gmail.com>
2023-06-15 14:15:58 +0100

    docs: format code

--- src/pages/[community_id].js
+++ src/pages/[community_id].js
@@ -1,9 +1,6 @@
 import Link from "next/link";
 import NavBar from "@/components/Header/NavBar";
-import CustomizableAvatar from "@/components/common/CustomizableAvatar";
-import CustomizableButton from "@/components/common/CustomizableButton";
 import FooterBar from "@/components/common/FooterBar";
-import Image from "next/image";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { getDocs, collection } from "firebase/firestore";
@@ -37,24 +34,17 @@ function SingleListing(className, text) {
     <>
       <NavBar />
       <CommunityHeader listing={listing} />
-
-
       <div className="bg-gradient-lite-grey flex justify-between p-[52px] mt-12 ">
-                  <h2 
-                   className ="text-grey-dark text-lg sm:text-lg lg:text-xl font-medium leading-[52px]"               
-                  >
-                    Would you like to be a part of this community?
-                  </h2> 
-                <Link 
-                href="/" 
-                className ="bg-gradient-lite-blue cursor-pointer rounded-md w-[250px] text-center px-6 py-2 text-white shadow-sm hover:bg-indigo-500"
-                >
-                  Visit Community
-                </Link>  
+        <h2 className="text-grey-dark text-lg sm:text-lg lg:text-xl font-medium leading-[52px]">
+          Would you like to be a part of this community?
+        </h2>
+        <Link
+          href="/"
+          className="bg-gradient-lite-blue cursor-pointer rounded-md w-[250px] text-center px-6 py-2 text-white shadow-sm hover:bg-indigo-500"
+        >
+          Visit Community
+        </Link>
       </div>
-
-
-
       <FooterBar />
     </>
   );
