a131dc916897b5a0c1928d1fab6bedcb8bb1d3c9
Ibimina hart <ibimina.c.hart@gmail.com>
2023-06-15 14:14:36 +0100

    docs: redirect user after login

--- src/composables/authLogIn.js
+++ src/composables/authLogIn.js
@@ -2,20 +2,20 @@ import { signInWithEmailAndPassword } from "firebase/auth";
 import { firebaseAuth, firebaseDb } from "@/firebase/config";
 import { useState } from "react";
 import useAuthContext from "@/context/useAuthContext";
+import { useRouter } from "next/router";
 
 function useAuthLogIn() {
   const { dispatch } = useAuthContext();
+  const router = useRouter();
 
   const [firebaseError, setFirebaseError] = useState("");
-
   const LogIn = ({ email, password }) => {
     signInWithEmailAndPassword(firebaseAuth, email, password)
       .then(async (userCredential) => {
-        // setAddUser(userCredential.user)
         const docRef = doc(firebaseDb, "MANAGERS", userCredential?.user?.uid);
         const docSnap = await getDoc(docRef);
         dispatch({ type: "SIGN_IN", payload: docSnap.data() });
-        //  Handle successful login
+        router.push("/managerDashboard");
       })
       .catch((error) => {
         setFirebaseError(error.message);
