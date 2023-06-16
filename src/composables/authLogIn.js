fd1a54914c99e09501ac662a104a019e780b2b3b
Ibimina hart <ibimina.c.hart@gmail.com>
2023-06-16 09:22:01 +0100

    docs: import firebase functions

--- src/composables/authLogIn.js
+++ src/composables/authLogIn.js
@@ -1,5 +1,6 @@
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { firebaseAuth, firebaseDb } from "@/firebase/config";
+import { doc, getDoc } from "firebase/firestore";
 import { useState } from "react";
 import useAuthContext from "@/context/useAuthContext";
 import { useRouter } from "next/router";
@@ -14,11 +15,12 @@ function useAuthLogIn() {
       .then(async (userCredential) => {
         const docRef = doc(firebaseDb, "MANAGERS", userCredential?.user?.uid);
         const docSnap = await getDoc(docRef);
-        dispatch({ type: "SIGN_IN", payload: docSnap.data() });
+        dispatch({ type: "SIGN_IN", payload: docSnap.data()});
         router.push("/managerDashboard");
       })
       .catch((error) => {
         setFirebaseError(error.message);
+        console.log(error.message);
       });
   };
   return { firebaseError, LogIn };
