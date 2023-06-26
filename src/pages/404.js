027bf3e51c26f328255d3ef448ea520719e25883
Neka Blair <ourblairfamily@gmail.com>
2023-06-26 09:10:37 -0500

    first commit, add image and home link

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,3 +1,20 @@
+import Link from "next/link";
+import Image from "next/image";
+
 export default function Custom404() {
-    return <h1 className="text-7xl py-80 text-footer-grey font-light flex justify-center items-center bg-gradient-to-r from-blue-500 min-h-screen">404 - Page Not Found</h1>
+    return (
+        <div className="text-7xl py-80 text-footer-grey font-light flex flex-col justify-center items-center gap-20 bg-gradient-to-r from-blue-500 min-h-screen">
+            <Image
+          src="/LOGO.png"
+          alt="Find Your People Tech Logo"
+          className="sm:h-full"
+          width={80}
+          height={28}
+          priority
+        />
+            <h1>404 - Page Not Found</h1>
+            <Link href={"/"}> Go Home</Link>
+        </div>
+    )
+    
 }
