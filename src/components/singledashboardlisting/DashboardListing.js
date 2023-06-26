7b3b16cdeb8bfd4157e1e40acc5f2dcce4c1f87e
Ibimina hart <ibimina.c.hart@gmail.com>
2023-06-26 11:56:03 +0100

    remove communty image

--- src/components/singledashboardlisting/DashboardListing.js
+++ src/components/singledashboardlisting/DashboardListing.js
@@ -3,21 +3,12 @@ import styles from "./dashboardlisting.module.css";
 import CustomizableButton from "../common/CustomizableButton";
 
 
-function DashboardListing({coummnity}) {
+function DashboardListing({community}) {
   return (
     <main className={`bg-white py-4 rounded-lg shadow-md col-span-1 p-2`}>
       <div>
-        <Image
-          src="/background.png"
-          className={styles.img}
-          height={100}
-          width={100}
-          alt="community photo"
-        />
-      </div>
-      <div>
         <p className='mb-3'>
-          Community Name: <span>Approved</span>
+          {community.community_name}: {community.approved ? "Approved" : "Pending"}
         </p>
         <div className={`flex items-center justify-center gap-4 mb-2`}>
           <CustomizableButton customClass={styles.view} text="View Page" />
