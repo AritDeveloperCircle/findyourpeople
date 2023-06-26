8bba99724596c12bdbef079cc942531dd2c0eaf7
Ibimina hart <ibimina.c.hart@gmail.com>
2023-06-26 10:44:20 +0100

    add header for form

--- src/pages/managerForm.js
+++ src/pages/managerForm.js
@@ -1,18 +1,16 @@
-import { useState} from "react";
+import { useState } from "react";
 import React from "react";
 import { useRouter } from "next/navigation";
-import NavBar from "@/components/Header/NavBar";
 import CustomizableButton from "@/components/common/CustomizableButton";
 import FooterBar from "@/components/common/FooterBar";
 import styles from "../styles/managerForm.module.css";
-import { firebaseDb} from "@/firebase/config";
-import {collection, addDoc} from "firebase/firestore";
+import { firebaseDb } from "@/firebase/config";
+import { collection, addDoc } from "firebase/firestore";
 import useAuthContext from "@/context/useAuthContext";
-
-
+import Link from "next/link";
 
 function ManagerForm() {
-  const {state} = useAuthContext();
+  const { state } = useAuthContext();
   const initialFormData = {
     community_name: "",
     community_manager: "",
@@ -86,14 +84,14 @@ function ManagerForm() {
       maxLength: 300,
       rows: 3,
       required: true,
-    }
+    },
   ];
   const submitCommunity = async (event) => {
     event.preventDefault();
     const commCollectionRef = collection(
       firebaseDb,
       "MANAGERS",
-     `${state?.user?.userid}`,
+      `${state?.user?.userid}`,
       "MANAGER_LISTINGS"
     );
     await addDoc(commCollectionRef, {
@@ -102,16 +100,32 @@ function ManagerForm() {
       setFormData(initialFormData);
     });
   };
-  
+
   return (
     <div className="">
-      <NavBar />
-      <h1 className="bg-gradient-lite-blue h-20 md:h-24 w-auto md:w-full flex flex-col justify-center items-center px-3.5 py-14 gap-2.5 font-bold text-4xl text-gray-lite">New Community</h1>
+      <header className="flex items-center justify-between px-4 py-4">
+        <h1>Findyourpeople.tech</h1>
+        <ul className="flex items-center gap-2">
+          <li>
+            <Link href="/managerDashboard">Back to Dashboard</Link>
+   
+          </li>
+          <li className="bg-yellow-200 px-4 py-2 rounded">
+            <button>logout</button>
+          </li>
+        </ul>
+      </header>
+      <h1 className="bg-gradient-lite-blue text-center py-4  font-bold text-4xl text-gray-lite">
+        New Community
+      </h1>
       <form className="px-4 p-2" onSubmit={submitCommunity}>
-        <div  className='grid lg:grid-cols-3 lg:gap-4'>
+        <div className="grid lg:grid-cols-3 lg:gap-4">
           {inputFields.map((field) => (
             <div key={field.name} className="mb-4">
-              <label className="ml-2 font-bold text-xl leading-9 text-gray-dark " htmlFor={field.name}>
+              <label
+                className="ml-2 font-bold text-xl leading-9 text-gray-dark "
+                htmlFor={field.name}
+              >
                 {field.label}
                 {field.required && <span className="text-red-500">*</span>}
               </label>
@@ -130,7 +144,6 @@ function ManagerForm() {
           ))}
         </div>
         <div className="flex justify-center items-center gap-2 my-4">
-
           <button
             className="cusor-pointer px-2 py-1 font-bold text-lg border text-primary border-primary rounded-lg  "
             onClick={() => router.push("/")}
@@ -146,7 +159,6 @@ function ManagerForm() {
             type="submit"
           />
         </div>
-     
       </form>
       <FooterBar />
     </div>
