365d65aadddbd94c97f739666c8ada73eb33a93c
Chibuzo Idia <caren.vhr@gmail.com>
2023-06-27 01:01:16 +0100

    removed an error

--- src/pages/managerForm.js
+++ src/pages/managerForm.js
@@ -14,7 +14,7 @@ import { uploadBytes, getDownloadURL, ref } from "firebase/storage";
 
 
 function ManagerForm() {
-  const initialFormData = {
+const initialFormData = {
     community_name: "",
     community_manager: "",
     community_location: "",
@@ -24,13 +24,13 @@ function ManagerForm() {
     community_date: "",
     community_vision: "",
     community_description: "",
-  };
-  const [formData, setFormData] = useState(initialFormData);
-  const fileInputRef = useRef(null);
-  const formRef = useRef(null);
-  const [fileUpload, setFileUpload] = useState();
-  const router = useRouter();
-  const handleChange = (event) => {
+};
+const [formData, setFormData] = useState(initialFormData);
+const fileInputRef = useRef(null);
+const formRef = useRef(null);
+const [fileUpload, setFileUpload] = useState();
+const router = useRouter();
+const handleChange = (event) => {
     event.preventDefault();
     setFormData({ ...formData, [event.target.name]: event.target.value });
   };
@@ -45,82 +45,82 @@ function ManagerForm() {
       name: "community_manager",
       label: "Community Manager",
       type: "text",
-      required: true,
+    required: true,
     },
     {
-      name: "community_url",
-      label: "Community URL",
-      type: "url",
-      placeholder: "https://example.com",
-      pattern: "https://.*",
-      required: true,
+    name: "community_url",
+    label: "Community URL",
+    type: "url",
+    placeholder: "https://example.com",
+    pattern: "https://.*",
+    required: true,
     },
     {
-      name: "manager_linkedin",
-      label: "Manager Linkedin",
-      type: "text",
-      required: false,
+    name: "manager_linkedin",
+    label: "Manager Linkedin",
+    type: "text",
+    required: false,
     },
     {
-      name: "manager_twitter",
-      label: "Manager Twitter",
-      type: "text",
-      required: false,
+    name: "manager_twitter",
+    label: "Manager Twitter",
+    type: "text",
+    required: false,
     },
     {
-      name: "community_date",
-      label: "Community Start Date",
-      type: "text",
-      required: false,
+    name: "community_date",
+    label: "Community Start Date",
+    type: "text",
+    required: false,
     },
     {
-      name: "community_vision",
-      label: "Community Vision",
-      type: "textarea",
-      minLength: 15,
-      maxLength: 300,
-      rows: 3,
-      required: false,
+    name: "community_vision",
+    label: "Community Vision",
+    type: "textarea",
+    minLength: 15,
+    maxLength: 300,
+    rows: 3,
+    required: false,
     },
     {
-      name: "community_description",
-      label: "Community Description",
-      type: "textarea",
-      minLength: 15,
-      maxLength: 300,
-      rows: 3,
-      required: true,
+    name: "community_description",
+    label: "Community Description",
+    type: "textarea",
+    minLength: 15,
+    maxLength: 300,
+    rows: 3,
+    required: true,
     }
-  ];
-  const submitCommunity = async (event) => {
+];
+const submitCommunity = async (event) => {
     event.preventDefault();
     const commCollectionRef = collection(
-      firebaseDb,
-      "MANAGERS",
-      "3fF17YaSFLHgRgksgVU8",
-      "MANAGER_LISTINGS"
+    firebaseDb,
+    "MANAGERS",
+    "3fF17YaSFLHgRgksgVU8",
+    "MANAGER_LISTINGS"
     );
     await addDoc(commCollectionRef, {
-      formData,
+    formData,
     }).then(() => {  
-      setFormData(initialFormData);
+    setFormData(initialFormData);
     });
     alert("Community added!")
-  };
-  
-  return (
+};
+
+return (
     <div className="overflow-y-hidden grid place-items-stretch w-[90rem] max-h-[132rem] lg:w-auto">
-      <NavBar />
-      <h1 className="bg-gradient-lite-blue h-20 md:h-24 w-[90rem] lg:w-auto flex flex-col justify-center items-center px-3.5 py-14 gap-2.5 font-bold text-4xl text-gray-lite">New Community</h1>
-      <form onSubmit={submitCommunity} action="/send-data-here" method="POST">
+    <NavBar />
+    <h1 className="bg-gradient-lite-blue h-20 md:h-24 w-[90rem] lg:w-auto flex flex-col justify-center items-center px-3.5 py-14 gap-2.5 font-bold text-4xl text-gray-lite">New Community</h1>
+    <form onSubmit={submitCommunity} action="/send-data-here" method="POST">
         <container className={styles.communityLayout}>
-          {inputFields.map((field) => (
+        {inputFields.map((field) => (
             <div className={styles.layoutRow} key={field.name}>
-              <label className="ml-2 font-bold text-xl leading-9 text-gray-dark w-52 h-9 md:w-64" htmlFor={field.name}>
+            <label className="ml-2 font-bold text-xl leading-9 text-gray-dark w-52 h-9 md:w-64" htmlFor={field.name}>
                 {field.label}
                 {field.required && <span className="text-red-500">*</span>}
-              </label>
-              <input
+            </label>
+            <input
                 type={field.type}
                 name={field.name}
                 id={field.name}
@@ -130,31 +130,31 @@ function ManagerForm() {
                 className="border-2 w-80 grid md:max-w-md h-9 border-gradient-lite-grey rounded-lg ms-0"
                 placeholder={field.placeholder}
                 pattern={field.pattern}
-              />
+            />
             </div>
-          ))}
+        ))}
         </container>
 
         <div className="px-36 py-3 flex flex-row justify-center items-center">
-          <button
+        <button
             className="w-44 sm:w-64 h-9 sm:h-14 px-2 font-bold text-lg border text-primary border-primary rounded-lg"
             onClick={() => router.push("/")}
             text="Return to Home"
             type="submit"
-          >
+        >
             Return to Home
-          </button>
-          <CustomizableButton
+        </button>
+        <CustomizableButton
             customClass={styles.communityBottomButtons}
             onClick={submitCommunity}
             text="Submit"
             type="submit"
-          />
+        />
         </div>
-      </form>
-      <FooterBar />
+    </form>
+    <FooterBar />
     </div>
-  );
+);
 }
 
 export default ManagerForm;
